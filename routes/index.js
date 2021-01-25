dcd3e3eb9f4919cae3b7fb3513fb80dd8ce4cc8c
mohamed Edrees <medoenoch@gmail.com>
2021-01-25 10:07:38 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const post = require('./post');
 const user = require('./user');
-const authMiddleware=require('../middelwares/auth');
+const authMiddleware=require('../middelwares/auth');//issue
 
 const router = express.Router();
 
-router.use('/posts',authMiddleware,post);
+router.use('/posts',post);
 router.use('/users',user);
 
 
