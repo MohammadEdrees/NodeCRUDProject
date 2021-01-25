739e80b404d558baff8a7e1cd7eca24ceffd091c
mohamed Edrees <medoenoch@gmail.com>
2021-01-25 20:14:06 +0200

    Update auth.js

--- middelwares/auth.js
+++ middelwares/auth.js
@@ -7,7 +7,9 @@ const auth = async (req,res,next)=>{
         next((new Error('UN_AUTHENTICATED')));
     }
     try{
-    const token =  await asyncVerify(authorization,'SECRET_MUST_BE_COMPLEX');
+    const id =  await asyncVerify(authorization,'SECRET_MUST_BE_COMPLEX');
+    const user = user.findById(id);
+    request.user=user;
         next();
     } catch(err){
         next((new Error('UN_AUTHENTICATED')));
