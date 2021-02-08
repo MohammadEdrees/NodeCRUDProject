85e19816c5aa1e80b2618def5ff996dfa373f861
mohamed Edrees <medoenoch@gmail.com>
2021-02-08 20:03:54 +0200

    Update auth.js

--- middelwares/auth.js
+++ middelwares/auth.js
@@ -4,20 +4,20 @@ const asyncVerify=promisify(jwt.verify);//transform to promise
 const auth = async (req,res,next)=>{
     const { headers: { authorization } } =req;
     if(!authorization){
+        console.log("non verified user");
         next((new Error('UN_AUTHENTICATED')));
     }
-    try{
+    
     const id =  await asyncVerify(authorization,'SECRET_MUST_BE_COMPLEX');
     const user = user.findById(id);
     console.log("verified user");
     req.user=user; //fixed 
     //if success next
+    console.log(" verified user");
     res.json(user);
         next();
-    } catch(err){
-        console.log("verified user");
-        next((new Error('UN_AUTHENTICATED')));
-    }
+        //next((new Error('UN_AUTHENTICATED')));
+    
 
 };
 module.exports=auth;
