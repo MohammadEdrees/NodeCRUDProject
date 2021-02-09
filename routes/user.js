9a04a8c5fc92de655abd61acd32176035907d95c
mohamed Edrees <medoenoch@gmail.com>
2021-02-10 01:14:43 +0200

    bug fix v2

--- routes/user.js
+++ routes/user.js
@@ -51,55 +51,55 @@ router.patch('/:id', async (req, res, next) => {
     }
 });
 
-// router.post("/follow/:id", authMiddleware, (req, res, next) => {
-//     if (req.user.id === req.params.user_id) {
-//         return res.status(400).json({ alreadyfollow: "You cannot follow yourself" })
-//     }
+router.post("/follow/:id", authMiddleware, (req, res, next) => {
+    if (req.user.id === req.params.user_id) {
+        return res.status(400).json({ alreadyfollow: "You cannot follow yourself" })
+    }
 
-//     User.findById(req.params.id)
-//         .then(user => {
-//             console.log(user)
-//             // check if the requested user is already in follower list of other user then 
+    User.findById(req.params.id)
+        .then(user => {
+            console.log(user)
+            // check if the requested user is already in follower list of other user then 
 
-//             if (user.followers.filter(follower =>
-//                 follower.user.toString() === req.user.id).length > 0) {
-//                 return res.status(400).json({ alreadyfollow: "You already followed the user" })
-//             }
+            if (user.followers.filter(follower =>
+                follower.user.toString() === req.user.id).length > 0) {
+                return res.status(400).json({ alreadyfollow: "You already followed the user" })
+            }
 
-//             user.followers.unshift({ user: req.user.id });
-//             user.save()
-//             User.findOne({ username: req.user.username })
-//                 .then(user => {
-//                     console.log(user)
-//                     user.following.unshift({ user: req.params.user_id });
-//                     user.save().then(user => res.json(user))
-//                 })
-//                 .catch(err => res.status(404).json({ alradyfollow: "you already followed the user" }))
-//         })
+            user.followers.unshift({ user: req.user.id });
+            user.save()
+            User.findOne({ username: req.user.username })
+                .then(user => {
+                    console.log(user)
+                    user.following.unshift({ user: req.params.user_id });
+                    user.save().then(user => res.json(user))
+                })
+                .catch(err => res.status(404).json({ alradyfollow: "you already followed the user" }))
+        })
 
-// })
+})
 
 
-// router.put('/unfollow/:unfollowId', authMiddleware, (req, res) => {
-//     User.findByIdAndUpdate(req.params.unfollowId, {
-//         $pull: { followers: req.user._id }
-//     }, {
-//         new: true
-//     }, (err, result) => {
-//         if (err) {
-//             return res.status(422).json({ error: err })
-//         }
-//         User.findByIdAndUpdate(req.user._id, {
-//             $pull: { following: req.params.unfollowId }
+router.put('/unfollow/:unfollowId', authMiddleware, (req, res) => {
+    User.findByIdAndUpdate(req.params.unfollowId, {
+        $pull: { followers: req.user._id }
+    }, {
+        new: true
+    }, (err, result) => {
+        if (err) {
+            return res.status(422).json({ error: err })
+        }
+        User.findByIdAndUpdate(req.user._id, {
+            $pull: { following: req.params.unfollowId }
 
-//         }, { new: true }).select("").then(result => {
-//             res.json(result)
-//         }).catch(err => {
-//             return res.status(422).json({ error: err })
-//         })
+        }, { new: true }).select("").then(result => {
+            res.json(result)
+        }).catch(err => {
+            return res.status(422).json({ error: err })
+        })
 
-//     })
-// });
+    })
+});
 
 
 
