0f3d168f92927450574f0a5a7bd068c542291f33
mohamed Edrees <medoenoch@gmail.com>
2021-02-10 01:13:18 +0200

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,82 +1,72 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+//create --------------------------------------------------------------
 const create = (user) => {
     return User.create(user);
 }
 
-
-//second change with no test :
+//Follow--------------------------------------------------------
 const follow = (userid, followid) => {
     User.findByIdAndUpdate(userid, { $addToSet: { following: followid } }, { new: true }).exec()
     User.findByIdAndUpdate(followid, { $addToSet: { follower: userid } }, { new: true }).exec()
 
     return ("status:followed");
 }
-
+//unfollow----------------------------------------------------
 const unfollow = (userid, followid) => {
     User.findByIdAndUpdate(userid, { $pull: { following: followid } }, { new: true }).exec()
     User.findByIdAndUpdate(followid, { $pull: { follower: userid } }, { new: true }).exec()
     return ("status:unfollowe");
 }
-
-const getfollowers = async(id) => {
+//All Followers ----------------------------------------------------
+const getfollowers = async (id) => {
     const { followers } = await getById(id)
     return User.find().where('_id').in(followers).exec();
 }
-
-const getfollowing = async(id) => {
-        const { following } = await getById(id)
-        return User.find().where('_id').in(following).exec();
-    }
-    //-----------------------------------------------
-const create = (user) => {
-    return User.create(user);
+// get this followers ------------------
+const getfollowing = async (id) => {
+    const { following } = await getById(id)
+    return User.find().where('_id').in(following).exec();
 }
+//-----------------------------------------------
 const getById = (id) => User.findById(id).exec();
+// All users---------------------------------
 const getAllUsers = () => User.find({});
+//edit------------------------------------------
 const editOne = (id, data) => User.findByIdAndUpdate(id, data, { new: true }).exec();
+//token ----------------------------------------------
 const { promisify } = require('util');
 const asyncSign = promisify(jwt.sign);
-const login = async({ mail, password }) => {
+
+
+//login
+const login = async ({ mail, password }) => {
+
     const user = await User.findOne({ mail }).exec();
     if (!user) {
         throw Error('UN_AUTHENTICATED');
     }
-    const getById = (id) => User.findById(id).exec();
-    const getAllUsers = () => User.find({});
-    const editOne = (id, data) => User.findByIdAndUpdate(id, data, { new: true }).exec();
-    const { promisify } = require('util');
-    const asyncSign = promisify(jwt.sign);
-    const login = async({ mail, password }) => {
-        const user = await User.findOne({ mail }).exec();
-        if (!user) {
-            throw Error('UN_AUTHENTICATED');
-        }
+    const isValidePass = user.validatePassword(password);
 
-        const isValidePass = user.validatePassword(password);
-
-        if (!isValidePass) {
-            throw Error('UN_AUTHENTICATED');
-        }
-        const token = await asyncSign({
-            mail: user.mail,
-            password: user.password
-        }, 'SECRET_MUST_BE_COMPLEX', { expiresIn: '10d' });
-
-        return {...user.toJSON(), expiresIn, token };
-        //return user;
+    if (!isValidePass) {
+        throw Error('UN_AUTHENTICATED');
     }
+    const token = await asyncSign({
+        mail: user.mail,
+        password: user.password
+    }, 'SECRET_MUST_BE_COMPLEX', { expiresIn: '1d' });
+    return { ...user.toJSON(), expiresIn, token };
 
-
-    module.exports = {
-        create,
-        getAllUsers,
-        login,
-        editOne,
-        getById,
-        follow,
-        unfollow,
-        getfollowers,
-        getfollowing
-    }
+}
+// Export models
+module.exports = {
+    create,
+    getAllUsers,
+    login,
+    editOne,
+    getById,
+    follow,
+    unfollow,
+    getfollowers,
+    getfollowing
 }
