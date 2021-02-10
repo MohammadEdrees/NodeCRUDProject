231dc3c1c3b3bccb048fe67ed523c0b830da6ba9
mohamed Edrees <medoenoch@gmail.com>
2021-02-10 20:33:29 +0200

    Update auth.js

--- middelwares/auth.js
+++ middelwares/auth.js
@@ -12,11 +12,11 @@ const auth = async (req,res,next)=>{
     }
     try{
     const id =  await asyncVerify(authorization,'SECRET_MUST_BE_COMPLEX');
-    res.json({case1:" line 13 ok"});
-    const user =  User.findById(id).exec();
-    res.json({case2:" line 15 ok"});
+   // res.json({case1:" line 13 ok"});
+   // const user = await User.findById(id).exec();
+   // res.json({case2:" line 15 ok"});
     //req.user=user; //fixed 
-     res.json({user});
+   //  res.json({user});
     res.json({case3:" line 18 ok"});
 
     next();
