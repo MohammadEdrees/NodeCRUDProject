bab7eb6ca6a450f2981c625f9125cb89d8cdd2e8
mohamed Edrees <medoenoch@gmail.com>
2021-02-10 13:08:35 +0200

    Update auth.js

--- middelwares/auth.js
+++ middelwares/auth.js
@@ -13,8 +13,8 @@ const auth = async (req,res,next)=>{
     const id =  await asyncVerify(authorization,'SECRET_MUST_BE_COMPLEX');
     const user = await User.findById(id).exec();
     console.log("verified user");
-    req.user=user; //fixed 
-    // res.json(user);
+    //req.user=user; //fixed 
+     res.json(user);
     next();
     }catch(e){
         next((new Error('UN_AUTHENTICATED')));
