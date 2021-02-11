25cfcc8dfd23b13263878cc3ee81fc7792de72f5
mohamed Edrees <medoenoch@gmail.com>
2021-02-12 00:03:47 +0200

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -6,7 +6,7 @@ const router = express.Router();
 //--------------------------------------
 var storage = multer.diskStorage({
     destination: function (req, res, cb) {
-        cb(null, 'images')
+        cb(null, 'images/')
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now());
