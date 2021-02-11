930702a2cfc3030f277da96d6341850ab5bc820c
mohamed Edrees <medoenoch@gmail.com>
2021-02-11 10:35:32 +0200

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -10,7 +10,7 @@ var storage = multer.diskStorage({
         cb(null,'/images')
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname)
+        cb(null, file.originalname + '-' + Date.now() )
     }
 })
 const upload = multer({
