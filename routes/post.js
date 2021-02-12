932a79696870ba4149eb25719c58bc6592db3a29
mohamed Edrees <medoenoch@gmail.com>
2021-02-12 02:48:31 +0200

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -16,7 +16,7 @@ cloudinary.config({
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary,
+    cloudinary: cloudinary,
     folder: 'images',
     allowedFormats: ['jpg', 'png'],
     transformation: [{ width: 500, height: 500, crop: 'limit' }]
