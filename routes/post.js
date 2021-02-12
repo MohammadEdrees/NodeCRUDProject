fb3c2ce9afb1be34938843d06fba4ff0c7026447
mohamed Edrees <medoenoch@gmail.com>
2021-02-13 01:37:18 +0200

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -107,7 +107,7 @@ router.delete('/:id', authMiddleware, async (req, res, next) => {
 
 });
 
-router.get('/post/:uid',(req,res,next)=>{
+router.get('/:uid',(req,res,next)=>{
     try{
     let currenUser = req.params.uid;
     const result = await currentUposts(currenUser);
