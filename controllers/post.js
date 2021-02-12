d36ba8ee323e7fa0bdb1320d9fab2a35e6185dc9
mohamed Edrees <medoenoch@gmail.com>
2021-02-13 00:51:34 +0200

    Update post.js

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,5 @@
 const Post = require('../models/Post');
-
+const User = require('../models/User');
 //Create blog--------------------------------------//
 const create=(post)=>{return Post.create(post);}
 //get All blogs--------------------------//
@@ -11,7 +11,7 @@ const edit =(id,body)=>Post.findByIdAndUpdate(id,body,{new:true}).exec();
 //delete by id -----------------------------------------//
 const deletP =(id)=>Post.findByIdAndRemove(id).exec();
 //PostsOfOneUser
-const currentUposts=(id)=>Post.find({' userId ' : id}).exec();
+const currentUposts=(id)=>Post.find(User._id === id).exec();
 
 module.exports={
     create,getAll,getById,edit,deletP,currentUposts
