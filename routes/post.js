db284b73c9d81f02a7b9bef212c8c2aeccfdccba
mohamed Edrees <medoenoch@gmail.com>
2021-02-13 18:24:00 +0200

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -114,7 +114,7 @@ router.put('/post', async(req, res, next) => {
     
     try {
       //  const blogs = await getAlll( {'mail' :{  "$regex" : mail } }  );
-        res.json(req.mail);
+        res.json(req.token);
     } catch (e) {
         next(e);
     }
