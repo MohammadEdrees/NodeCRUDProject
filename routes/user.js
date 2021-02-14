08fda044963ac77c39786294136d247f6e652cca
mohamed Edrees <medoenoch@gmail.com>
2021-02-14 04:14:34 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -83,7 +83,7 @@ router.put('/unfollow/:id', authMiddleware, (req, res) => {
     const currentUser= user.id;
     const FollowedOne= id ;
     //check i is not my id
-    if((FollowedOne == currentUser)){
+    if((FollowedOne != currentUser)){
         if(user.following.includes(FollowedOne)){
             //unfollow
             res.json("1");
@@ -97,7 +97,7 @@ router.put('/unfollow/:id', authMiddleware, (req, res) => {
             res.json({msg:"He is not in your follwing list "});
         }
     }else{
-        res.json({ msg :" Not same "});
+        res.json({ msg :" same "});
     }
     //else cheer him 
     
