4d7eaeab2e75148c7d8577e6f6a7f0de99ba65f1
mohamed Edrees <medoenoch@gmail.com>
2021-02-23 18:39:37 +0200

    token

--- middelwares/auth.js
+++ middelwares/auth.js
@@ -4,21 +4,18 @@ const User = require('../models/User.js');
 const asyncVerify = promisify(jwt.verify); //transform to promise
 
 const auth = async(req, res, next) => {
-    // try spread it ...auth = > { Access token - refresh token }
     const { headers: { authorization } } = req;
     if (!authorization) {
         next((new Error('UN_AUTHENTICATED')));
-       // res.json({ msg: "Confirm You Are Logged in Please.." });
     }
     try {
         const { id } = await asyncVerify(authorization, 'SECRET_MUST_BE_COMPLEX');
-       // res.json(id);
         const user = await User.findById(id).exec();
         req.user = user;
         next();
     } catch (e) {
          next((new Error('UN_AUTHENTICATED')));
-        //res.json({ msg: "login but can't access "});
+
 
     }
 };
