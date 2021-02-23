94de1b52b48558a84ff91fff7a84beda2e209c4f
mohamed Edrees <medoenoch@gmail.com>
2021-02-23 11:58:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Credentials','true');
+
     res.setHeader(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
