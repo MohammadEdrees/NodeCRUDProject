c11882b69d9c267f3543bf01a9d0068b36198690
mohamed Edrees <medoenoch@gmail.com>
2021-02-23 15:32:53 +0200

    lesson5

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const upload = multer({
 
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://myfirstnode7.herokuapp.com");
 
     res.setHeader(
         "Access-Control-Allow-Headers",
@@ -47,6 +47,7 @@ app.use('*', (req, res, next) => {
     res.status(404).json({ err: 'Not Found Error' });
 
 });
+
 
 //Err Handler
 app.use((err, req, res, next) => {
